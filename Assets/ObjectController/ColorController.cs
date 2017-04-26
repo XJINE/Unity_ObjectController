8855db06cdb5a2941f23f824d19509b7daf6b408
umeda <umeda@team-lab.com>
2017-04-26 15:42:17 +0900

    ColorController を追加。

---
+++ Assets/ObjectController/ColorController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace ObjectController
+{
+    /// <summary>
+    /// 色を制御します。
+    /// </summary>
+    public class ColorController : MonoBehaviour
+    {
+        #region Field
+
+        /// <summary>
+        /// 共有される MaterialPropertyBlock.
+        /// </summary>
+        protected static MaterialPropertyBlock SharedMaterialPropertyBlock = new MaterialPropertyBlock();
+
+        /// <summary>
+        /// 設定する色。
+        /// </summary>
+        public Color color;
+
+        /// <summary>
+        /// このオブジェクトのレンダラ。
+        /// </summary>
+        protected new Renderer renderer;
+
+        #endregion Field
+
+        /// <summary>
+        /// 開始時に呼び出されます。
+        /// </summary>
+        protected virtual void Start()
+        {
+            this.renderer = base.GetComponent<Renderer>();
+            SetColor(this.color);
+        }
+
+        /// <summary>
+        /// 色を設定します。
+        /// </summary>
+        /// <param name="color">
+        /// 設定する色。
+        /// </param>
+        public virtual void SetColor(Color color)
+        {
+            this.renderer.GetPropertyBlock(SharedMaterialPropertyBlock);
+
+            ColorController.SharedMaterialPropertyBlock.SetColor("_Color", color);
+
+            this.renderer.SetPropertyBlock(ColorController.SharedMaterialPropertyBlock);
+        }
+    }
+}
